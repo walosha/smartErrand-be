5789b392bc03936ae898713593d2bee009fa064e
olawale afuye <walosha@yahoo.com>
2023-03-14 02:35:18 +0100

    pushed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { v4 as uuidv4 } from "uuid";
-import { data } from "../text";
+import { data } from "../t";
 
 console.log({ data });
 
--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -42,20 +42,94 @@ export class LocationService {
   }
 
   async getVehiclesNearby(location, radius) {
-    const passengerIndex = latLngToCell(
-      3.099517822265625,
-      6.502263597007461,
-      10
-    );
+    const client_h3_index = await this.prisma.$queryRaw`
+   SELECT ST_GeoHash(ST_SetSRID(ST_AsText("coords"), 4326), 9) AS h3_index FROM "Location" WHERE id='fd220017-0da7-4f93-a6b9-6d9a19dc7645';
+`;
+    console.log("client_h3_index------------", client_h3_index[0]);
 
-    const hexBoundary = cellToBoundary(passengerIndex);
-    const [Lon, Lat] = hexBoundary[0];
-    console.log({ Lon, Lat });
+    const errand_h3_index = await this.prisma.$queryRaw`
+    SELECT "id", "address", ST_X("coords") as longitude, ST_Y("coords") as latitude, ST_GeoHash("coords", 9) as vehicle_h3_index
+    FROM "Location" WHERE st_distancesphere(
+        ST_AsText("coords"),
+        ST_MakePoint(7.0000,4.7500)  ) <= 30000 LIMIT  10 -- 1 mile in meters
+  `;
+
+    console.log("errand_h3_index------------", errand_h3_index);
+
+    const res = await this.prisma.$queryRaw`;
+             SELECT
+  v.id,
+  ST_Distance(
+    ST_SetSRID(ST_MakePoint(v.longitude, v.latitude), 4326),
+    ST_SetSRID(ST_MakePoint(p.passenger_longitude, p.passenger_latitude), 4326)
+  ) AS distance
+FROM
+  (
+    SELECT
+      id,
+      ST_X(coords) AS longitude,
+      ST_Y(coords) AS latitude
+    FROM
+      "Location"
+    WHERE
+      ST_Contains(
+        ST_SetSRID(
+          ST_GeomFromGeoHash(${client_h3_index[0].h3_index})::geometry,
+          4326
+        ),
+        coords
+      )
+  ) AS v
+  CROSS JOIN (
+    SELECT
+      ST_X(coords) AS passenger_longitude,
+      ST_Y(coords) AS passenger_latitude
+    FROM
+      "Location"
+    WHERE
+      id = 'fd220017-0da7-4f93-a6b9-6d9a19dc7645'
+  ) AS p
+ORDER BY
+  distance
+LIMIT
+  10;
+
+    `;
+
+    console.log("res------------", res);
+
+    const closestVehicle = await this.prisma.$queryRaw`
+  SELECT vehicle_id, ST_Distance(
+    ST_SetSRID(ST_Point("longitude", latitude), 4326),
+    ST_SetSRID(ST_Point("longitude", latitude), 4326)
+  ) AS distance
+  FROM (
+    SELECT "id", ST_X("coords") as longitude, ST_Y("coords") as latitude
+    FROM "Location"
+    WHERE  ST_Contains(
+        ST_SetSRID(
+          ST_GeomFromGeoHash(${client_h3_index[0].h3_index})::geometry,
+          4326
+        ),
+        coords
+      )
+  ) as data
+  CROSS JOIN (
+    SELECT ST_X(ST_SetSRID(ST_MakePoint("longitude", "latitude"), 4326)) as longitude,
+           ST_Y(ST_SetSRID(ST_MakePoint("longitude", "latitude"), 4326)) as latitude
+    FROM "Location"
+    WHERE passenger_id =${res[0].id}
+  ) as passenger_data
+  ORDER BY distance
+  LIMIT 1;
+`;
+
+    console.log("closestVehicle------------", closestVehicle);
 
     const result = await this.prisma.$queryRaw`
       SELECT "id","longitude","latitude","address", ST_AsText("coords") AS "coords" FROM "Location" WHERE st_distancesphere(
         ST_AsText("coords"),
-        ST_MakePoint(3.3390592,6.604070)  ) <= 4000 -- 1 mile in meters
+        ST_MakePoint(7.0000,4.7500)  ) <= 30000 LIMIT  10 -- 1 mile in meters
 `;
 
     // console.log({ vehicles });
@@ -64,7 +138,6 @@ export class LocationService {
     //      WHERE ST_DWithin(coords, ST_SetSRID(ST_MakePoint(3.099517822265625,6.502263597007461), 4326), .3)
     // `;
 
-    console.log({ raw: result });
     return result;
   }
 }
